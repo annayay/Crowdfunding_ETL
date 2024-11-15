36bba6f1f0fca51c902a9f71acd5857c9d06d993
annayay <annayay@gmail.com>
2024-11-14 21:20:19 -0500

    anna upload

---
+++ Starter_Files/Resources/crowdfunding_db_schema.sql
@@ -0,0 +1,56 @@
+Drop Table if exists "campaign";
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(255)   NOT NULL,
+    "description" TEXT   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(50)   NOT NULL,
+    "currency" VARCHAR(10)   NOT NULL,
+    "launch_date" DATE  NOT NULL,
+    "end_date" DATE  NOT NULL,
+    "category_id" VARCHAR(50)   NOT NULL,
+    "subcategory_id" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "subcategories" (
+    "subcategory_id" VARCHAR(50)   NOT NULL,
+    "subcategory" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_subcategories" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(50)   NOT NULL,
+    "category_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategories" ("subcategory_id");
